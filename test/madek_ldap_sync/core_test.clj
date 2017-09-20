1889944f87b96ea3234a4f0b596f61e62fcafb7b
Thomas Schank <DrTom@schank.ch>
2017-09-20 12:38:27 +0200

    Initial commit

---
+++ test/madek_ldap_sync/core_test.clj
@@ -0,0 +1,7 @@
+(ns madek-ldap-sync.core-test
+  (:require [clojure.test :refer :all]
+            [madek-ldap-sync.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
