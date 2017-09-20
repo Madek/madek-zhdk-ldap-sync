71cc8709fcbf4cfdd422cae8de5fb09e0a68fb26
Thomas Schank <DrTom@schank.ch>
2017-09-20 15:31:42 +0200

    Optionally reading data from file instead LDAP

---
+++ src/madek/data_file.clj
@@ -0,0 +1,20 @@
+(ns madek.data-file
+  (:require
+    [cheshire.core :as cheshire]
+
+    [clojure.tools.logging :as logging]
+    ))
+
+(defn run-write [data options]
+  (let [filename (:output-file options)]
+    (logging/info (str "Writing data to " filename " ...."))
+    (spit filename (cheshire/generate-string data {:pretty true}))
+    (logging/info (str "Writing data to " filename " done."))))
+
+(defn run-read [options]
+  (let [filename (:input-file options)]
+    (logging/info (str "Reading data from " filename " ...."))
+    (let [data (-> (slurp filename)
+                   (cheshire/parse-string true))]
+      (logging/info (str "Reading data from " filename " done."))
+      data)))
--- src/madek/main.clj
+++ src/madek/main.clj
@@ -1,7 +1,7 @@
 (ns madek.main
   (:require
     [madek.ldap-fetch :as ldap-fetch]
-    [madek.write-output-file :as write-output-file]
+    [madek.data-file :as data-file]
 
     [clojure.pprint :refer [pprint]]
     [clojure.tools.cli :refer [parse-opts]]
@@ -20,6 +20,7 @@
   [["-h" "--help"]
    ["-t" "--madek-token MADEK_TOKEN" "Token used to authenticate against the Madek server." :default (System/getenv "MADEK_TOKEN")]
    ["-u" "--madek-base-url BASE_URL" "Base URL of the Madek instance." :default "https://test.madek.zhdk.ch"]
+   [nil "--input-file INOUT_FILE" "The data will be retrieved from this file instead of fetching it from LDAP"]
    [nil "--output-file OUTPUT_FILE" "The data to be synced will be written to this json file instead." :default (System/getenv "OUTPUT_FILE")]
    [nil "--ldap-host" "Hostname/ip of the LDAP server" :default "adc3.ad.zhdk.ch"]
    [nil "--ldap-bind-dn BIND-DN" :default "CN=madeksvc,OU=Service Accounts,OU=Accounts,OU=_ZHdK manuell,DC=ad,DC=zhdk,DC=ch"]
@@ -60,8 +61,9 @@
   (catcher/snatch
     {:return-fn (fn [_] (System/exit 0))}
     (logging/info "Running Madek LDAP Sync ....")
-    (let [data (ldap-fetch/run options)]
+    (let [data (if (:input-file options)
+                 (data-file/run-read options)
+                 (ldap-fetch/run options))]
       (when (:output-file options)
-        (write-output-file/run data options)
-        ))
+        (data-file/run-write data options)))
     (logging/info "Running Madek LDAP Sync done.")))
